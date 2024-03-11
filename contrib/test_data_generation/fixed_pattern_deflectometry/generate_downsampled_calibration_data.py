2de72ada88b9e8426c2b6ef34bb15dbad2f2f5e2
Braden <bsmith4@sandia.gov>
2024-03-11 09:22:34 -0600

    fpd cal test generation data does not downsample images

--- contrib/test_data_generation/fixed_pattern_deflectometry/generate_downsampled_calibration_data.py
+++ contrib/test_data_generation/fixed_pattern_deflectometry/generate_downsampled_calibration_data.py
@@ -4,15 +4,15 @@ pattern dots.
 from glob import glob
 from os.path import join, basename
 import shutil
-import sys
+# import sys
 
 import imageio.v3 as imageio
 
 from opencsp.common.lib.opencsp_path.opencsp_root_path import opencsp_code_dir
 from opencsp.common.lib.photogrammetry import photogrammetry as ph
 
-sys.path.append(join(opencsp_code_dir(), '..'))
-import contrib.test_data_generation.downsample_data_general as ddg  # nopep8
+# sys.path.append(join(opencsp_code_dir(), '..'))
+# import contrib.test_data_generation.downsample_data_general as ddg  # nopep8
 
 
 def generate_data():
@@ -24,33 +24,32 @@ def generate_data():
 
     files_images = glob(join(dir_sample_data, 'images/*.JPG'))
     file_camera_cal = join(dir_sample_data, 'camera_calibration.h5')
-    file_camera_def = join(dir_sample_data, 'camera_deflectometry.h5')
-    file_camera_image = join(dir_sample_data, 'image_deflectometry_camera.png')
     file_point_locs = join(dir_sample_data, 'point_locations.csv')
 
     dir_save = join(opencsp_code_dir(),
                     'test/data/fixed_pattern_deflectometry_measurements/dot_location_calibration')
 
     # Downsample marker/dot images
-    n_downsample = 5
+    # n_downsample = 5
     for file in files_images:
         print(f'Processing {basename(file):s}...')
         # Load image
-        im = ph.load_image_grayscale(file)[..., None]
+        # im = ph.load_image_grayscale(file)[..., None]
+        im = ph.load_image_grayscale(file)
         # Downsample
-        im_ds = ddg.downsample_images(im, n_downsample)
+        # im_ds = ddg.downsample_images(im, n_downsample)
         # Save
         file_save = join(dir_save, 'images', basename(file))
-        imageio.imwrite(file_save, im_ds, quality=80)
+        # imageio.imwrite(file_save, im_ds, quality=80)
+        imageio.imwrite(file_save, im, quality=80)
 
     # Downsample cal camera
     print('Downsampling calibration camera...')
-    cam_cal = ddg.downsample_camera(file_camera_cal, n_downsample)
-    cam_cal.save_to_hdf(join(dir_save, basename(file_camera_cal)))
+    # cam_cal = ddg.downsample_camera(file_camera_cal, n_downsample)
+    # cam_cal.save_to_hdf(join(dir_save, basename(file_camera_cal)))
 
     # Save other files
-    shutil.copy(file_camera_def, join(dir_save, basename(file_camera_def)))
-    shutil.copy(file_camera_image, join(dir_save, basename(file_camera_image)))
+    shutil.copy(file_camera_cal, join(dir_save, basename(file_camera_cal)))
     shutil.copy(file_point_locs, join(dir_save, basename(file_point_locs)))
 
 
