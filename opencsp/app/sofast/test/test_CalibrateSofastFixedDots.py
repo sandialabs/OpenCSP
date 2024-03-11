90f0e2996fc6acdb32da36713e79fd6f8360fdfa
Braden <bsmith4@sandia.gov>
2024-03-11 11:52:55 -0600

    Added FixedPatternSetupCalibrate unit test.

--- opencsp/app/sofast/test/test_CalibrateSofastFixedDots.py
+++ opencsp/app/sofast/test/test_CalibrateSofastFixedDots.py
@@ -64,13 +64,10 @@ def test_FixedPatternSetupCalibrate():
     camera_marker = Camera.load_from_hdf(file_camera_marker)
 
     # Perform dot location calibration
-    cal_dot_locs = CalibrateSofastFixedDots(
+    cal_dot_locs = FixedPatternSetupCalibrate(
         files, origins, camera_marker, pts_xyz_corners, ids_corners, -32, 31, -31, 32
     )
     cal_dot_locs.plot = True
-    cal_dot_locs.blob_search_threshold = 3.
-    cal_dot_locs.blob_detector.minArea = 3.
-    cal_dot_locs.blob_detector.maxArea = 30.
     cal_dot_locs.run()
 
     # Save data
