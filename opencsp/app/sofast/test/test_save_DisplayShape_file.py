927730557fa8f73ab9b6b383d43761dffb48d330
Braden <bsmith4@sandia.gov>
2024-03-14 12:21:08 -0600

    Added debug string to test_save_displayShape

--- opencsp/app/sofast/test/test_save_DisplayShape_file.py
+++ opencsp/app/sofast/test/test_save_DisplayShape_file.py
@@ -1,4 +1,5 @@
 from os.path import join
+import unittest
 
 import numpy as np
 
@@ -12,37 +13,37 @@ from opencsp.common.lib.opencsp_path.opencsp_root_path import opencsp_code_dir
 import opencsp.common.lib.tool.file_tools as ft
 
 
-def test_save_physical_setup_file():
-    """Loads data and saves test Display file"""
-    # Define input file directory
-    base_dir = join(opencsp_code_dir(), 'common/lib/deflectometry/test')
-    dir_input = join(base_dir, 'data', 'data_expected')
-    dir_output = join(base_dir, 'data', 'output')
-    file_save = join(dir_output, 'test_physical_setup_file.h5')
+class test_save_physical_setup_file(unittest.TestCase):
+    def test_save_physical_setup_file(self):
+        """Loads data and saves test Display file"""
+        # Define input file directory
+        base_dir = join(opencsp_code_dir(), 'common/lib/deflectometry/test')
+        dir_input = join(base_dir, 'data', 'data_expected')
+        dir_output = join(base_dir, 'data', 'output')
+        file_save = join(dir_output, 'test_physical_setup_file.h5')
 
-    ft.create_directories_if_necessary(dir_output)
+        ft.create_directories_if_necessary(dir_output)
 
-    # Define data files
-    file_screen_distortion_data = join(dir_input, 'screen_distortion_data_100_100.h5')
-    file_cam = join(dir_input, 'camera_rvec_tvec.csv')
+        # Define data files
+        file_screen_distortion_data = join(dir_input, 'screen_distortion_data_100_100.h5')
+        file_cam = join(dir_input, 'camera_rvec_tvec.csv')
 
-    # Load data
-    name = 'Test Physical Setup File'
-    data_dist = load_hdf5_datasets(
-        ['pts_xy_screen_fraction', 'pts_xyz_screen_coords'], file_screen_distortion_data
-    )
-    screen_distortion_data = {
-        'pts_xy_screen_fraction': Vxy(data_dist['pts_xy_screen_fraction']),
-        'pts_xyz_screen_coords': Vxyz(data_dist['pts_xyz_screen_coords']),
-    }
-    data_cam = np.loadtxt(file_cam, delimiter=',')
-    rvec = data_cam[0]
-    tvec = data_cam[1]
+        # Load data
+        name = 'Test Physical Setup File'
+        data_dist = load_hdf5_datasets(
+            ['pts_xy_screen_fraction', 'pts_xyz_screen_coords'], file_screen_distortion_data
+        )
+        screen_distortion_data = {
+            'pts_xy_screen_fraction': Vxy(data_dist['pts_xy_screen_fraction']),
+            'pts_xyz_screen_coords': Vxyz(data_dist['pts_xyz_screen_coords']),
+        }
+        data_cam = np.loadtxt(file_cam, delimiter=',')
+        rvec = data_cam[0]
+        tvec = data_cam[1]
 
-    # Save physical setup file
-    save_DisplayShape_file(screen_distortion_data, name, rvec, tvec, file_save)
+        # Save physical setup file
+        save_DisplayShape_file(screen_distortion_data, name, rvec, tvec, file_save)
 
 
 if __name__ == '__main__':
-    test_save_physical_setup_file()
-    print('Test physical setup file save successfully.')
+    unittest.main()
