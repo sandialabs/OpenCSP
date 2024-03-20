3a272110cba849db3e2816d1f2ac1fd2c7859211
Braden <bsmith4@sandia.gov>
2024-03-20 16:12:47 -0600

    Fixed calibration unit tests

--- opencsp/app/sofast/test/test_CalibrateDisplayShape.py
+++ opencsp/app/sofast/test/test_CalibrateDisplayShape.py
@@ -1,7 +1,7 @@
 """Tests Sofast screen distortion calibration
 """
 
-from os.path import join
+from os.path import join, dirname
 import unittest
 
 from glob import glob
@@ -20,6 +20,8 @@ from opencsp.common.lib.camera.Camera import Camera
 from opencsp.common.lib.deflectometry.ImageProjection import ImageProjection
 from opencsp.common.lib.geometry.Vxyz import Vxyz
 from opencsp.common.lib.tool.hdf5_tools import load_hdf5_datasets
+import opencsp.common.lib.tool.file_tools as ft
+import opencsp.common.lib.tool.log_tools as lt
 
 
 class TestCalibrateDisplayShape(unittest.TestCase):
@@ -30,37 +32,23 @@ class TestCalibrateDisplayShape(unittest.TestCase):
         on the dir_input path.
         """
         # Define default data directories
-        dir_input_sofast = join(
-            opencsp_code_dir(), 'app/sofast/test/data/data_measurement'
-        )
-        dir_input_def = join(
-            opencsp_code_dir(), 'common/lib/deflectometry/test/data/data_measurement'
-        )
+        dir_input_sofast = join(opencsp_code_dir(), 'app/sofast/test/data/data_measurement')
+        dir_input_def = join(opencsp_code_dir(), 'common/lib/deflectometry/test/data/data_measurement')
         dir_output = join(opencsp_code_dir(), 'app/sofast/test/data/data_expected')
-
-        verbose = 1  # 0=no output, 1=only print outputs, 2=print outputs and show plots, 3=plots only with no printing
 
         # Define input files
         resolution_xy = [100, 100]  # sample density of screen
-        file_screen_cal_point_pairs = join(
-            dir_input_sofast, 'screen_calibration_point_pairs.csv'
-        )
+        file_screen_cal_point_pairs = join(dir_input_sofast, 'screen_calibration_point_pairs.csv')
         file_point_locations = join(dir_input_def, 'point_locations.csv')
         file_camera_distortion = join(dir_input_sofast, 'camera_screen_shape.h5')
         file_image_projection = join(dir_input_sofast, 'image_projection.h5')
-        files_screen_shape_measurement = glob(
-            join(dir_input_sofast, 'screen_shape_sofast_measurements/pose_*.h5')
-        )
+        files_screen_shape_measurement = glob(join(dir_input_sofast, 'screen_shape_sofast_measurements/pose_*.h5'))
 
         # Load input data
-        pts_marker_data = np.loadtxt(
-            file_point_locations, delimiter=',', dtype=float, skiprows=1
-        )
+        pts_marker_data = np.loadtxt(file_point_locations, delimiter=',', dtype=float, skiprows=1)
         pts_xyz_marker = Vxyz(pts_marker_data[:, 2:].T)
         corner_ids = pts_marker_data[:, 1]
-        screen_cal_point_pairs = np.loadtxt(
-            file_screen_cal_point_pairs, delimiter=',', skiprows=1
-        ).astype(int)
+        screen_cal_point_pairs = np.loadtxt(file_screen_cal_point_pairs, delimiter=',', skiprows=1).astype(int)
         camera = Camera.load_from_hdf(file_camera_distortion)
         image_projection_data = ImageProjection.load_from_hdf(file_image_projection)
 
@@ -77,7 +65,7 @@ class TestCalibrateDisplayShape(unittest.TestCase):
 
         # Perform screen position calibration
         cal_screen_position = CalibrateDisplayShape(data_input)
-        cal_screen_position.run_calibration(verbose)
+        cal_screen_position.run_calibration()
 
         # Get distortion data
         dist_data = cal_screen_position.get_data()
@@ -104,8 +92,10 @@ class TestCalibrateDisplayShape(unittest.TestCase):
             rtol=0,
             atol=1e-6,
         )
-        print('Distortion data tested successfully.')
 
 
 if __name__ == '__main__':
+    save_dir = join(dirname(__file__), 'data/output')
+    ft.create_directories_if_necessary(save_dir)
+    lt.logger(join(save_dir, 'log_display_shape.txt'), lt.log.WARN)
     unittest.main()
