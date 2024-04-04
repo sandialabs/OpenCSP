525a6f6c424e6ce7e07fce7a921249f7ff00ac8a
Braden <bsmith4@sandia.gov>
2024-04-04 16:32:54 -0600

    Fixed example_create_measurement_file_from_image by adding DistanceOpticScreen usage

--- example/sofast_fixed/example_measurement_from_image.py
+++ example/sofast_fixed/example_measurement_from_image.py
@@ -3,6 +3,7 @@ from os.path import join, dirname
 import cv2 as cv
 
 import opencsp.app.sofast.lib.image_processing as ip
+from opencsp.app.sofast.lib.DistanceOpticScreen import DistanceOpticScreen
 from opencsp.app.sofast.lib.MeasurementSofastFixed import MeasurementSofastFixed
 from opencsp.common.lib.geometry.Vxyz import Vxyz
 from opencsp.common.lib.opencsp_path.opencsp_root_path import opencsp_code_dir
@@ -67,7 +68,8 @@ def example_create_measurement_file_from_image():
 
     # 4. Create measurement object
     # ============================
-    measurement = MeasurementSofastFixed(image, v_measure_point_facet, dist_optic_screen, origin, name=name)
+    dist_optic_screen_measure = DistanceOpticScreen(v_measure_point_facet, dist_optic_screen)
+    measurement = MeasurementSofastFixed(image, dist_optic_screen_measure, origin, name=name)
 
     # 5. Save measurement as HDF5 file
     # ================================
