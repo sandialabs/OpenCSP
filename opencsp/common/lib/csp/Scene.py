f65e2f064e8e64a2083057195423174769210fef
tjlarki <tjlarki@sandia.gov>
2024-05-21 09:40:21 -0600

    added draw_onbjects to Scene

--- opencsp/common/lib/csp/Scene.py
+++ opencsp/common/lib/csp/Scene.py
@@ -3,6 +3,7 @@ from opencsp.common.lib.csp.LightSource import LightSource
 from opencsp.common.lib.csp.RayTraceable import RayTraceable
 from opencsp.common.lib.csp.OpticOrientationAbstract import OpticOrientationAbstract
 from opencsp.common.lib.geometry.TransformXYZ import TransformXYZ
+from opencsp.common.lib.render.View3d import View3d
 
 
 class Scene(OpticOrientationAbstract):
@@ -19,15 +20,44 @@ class Scene(OpticOrientationAbstract):
 
     def set_position_in_space(self, child: OpticOrientationAbstract,
                               transform: TransformXYZ) -> None:
+        """
+        Allows for the spacial placement of children inside the scene.
+        """
         if child not in self.children:
             raise ValueError(f"{child} is not a child of the Scene.")
         child._self_to_parent_transform = transform
 
+    # override from OpticOrientationAbstract
     @property
     def children(self) -> list[OpticOrientationAbstract]:
         return self.objects
 
+    # override from OpticOrientationAbstract
     def _add_child_helper(self, new_child: OpticOrientationAbstract):
         if isinstance(new_child, type(self)):
             raise ValueError(f"cannot add a Scene object {new_child} to a scene.")
         self.objects.append(new_child)
+
+    def draw_objects(self, view: View3d, render_controls: dict = None):
+        """
+        Will draw every `OpticOrientationAbstract` object in the scene. 
+        It determines the render control by taking the type of the object and 
+        looking for that type in the dictionary of render controls.
+
+        Parameters
+        ----------
+        view: View3d
+            - the view to draw the objects inside
+        
+        render_controls: dict[type, RenderControl]
+            - The render control objects that correspond to specific types 
+            - TODO this can be improved
+        """
+        if render_controls is None:
+            render_controls = {}
+
+        render_controls.setdefault(None, None)
+
+        for optic in self.objects:
+            style = render_controls[type(optic)]
+            optic.draw(view, style)
