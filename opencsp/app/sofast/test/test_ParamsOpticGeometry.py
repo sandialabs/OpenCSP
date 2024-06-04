89f64a64c548f9b1eb157c48eaae2a4e23390d49
Braden <bsmith4@sandia.gov>
2024-06-04 10:12:57 -0600

    Added HDF IO to ParamsOpticGeometry

--- opencsp/app/sofast/test/test_ParamsOpticGeometry.py
+++ opencsp/app/sofast/test/test_ParamsOpticGeometry.py
@@ -1,25 +1,25 @@
 import unittest
 from os.path import join, dirname
 
-from opencsp.app.sofast.lib.ParamsSofastFringe import ParamsSofastFringe
+from opencsp.app.sofast.lib.ParamsOpticGeometry import ParamsOpticGeometry
 import opencsp.common.lib.tool.file_tools as ft
 
 
-class TestParamsSofastFringe(unittest.TestCase):
+class TestParamsOpticGeometry(unittest.TestCase):
     def test_save_load_hdf(self):
         # Define save dir
-        dir_save = join(dirname(__file__), 'data/output/ParamsSofastFringe')
+        dir_save = join(dirname(__file__), 'data/output/ParamsOpticGeometry')
         ft.create_directories_if_necessary(dir_save)
-        file_save = join(dir_save, 'params_sofast_fringe.h5')
+        file_save = join(dir_save, 'params_optic_geometry.h5')
 
         # Instantiate with defaults
-        params = ParamsSofastFringe()
+        params = ParamsOpticGeometry()
 
         # Test save
         params.save_to_hdf(file_save)
 
         # Test load
-        ParamsSofastFringe.load_from_hdf(file_save)
+        ParamsOpticGeometry.load_from_hdf(file_save)
 
 
 if __name__ == '__main__':
