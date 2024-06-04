5a1d48804a5457c97e7b424b398bc3f5e5dd2983
Braden <bsmith4@sandia.gov>
2024-06-04 10:11:09 -0600

    Added HDF IO to ParamsSofastFringe

--- opencsp/app/sofast/lib/ParamsSofastFringe.py
+++ opencsp/app/sofast/lib/ParamsSofastFringe.py
@@ -3,15 +3,14 @@
 
 from dataclasses import dataclass, field
 
-
-from opencsp.app.sofast.lib.ParamsOpticGeometry import ParamsOpticGeometry
 from opencsp.app.sofast.lib.DebugOpticsGeometry import DebugOpticsGeometry
+from opencsp.app.sofast.lib.ParamsOpticGeometry import ParamsOpticGeometry
 from opencsp.common.lib.deflectometry.SlopeSolverDataDebug import SlopeSolverDataDebug
-import opencsp.common.lib.tool.hdf5_tools as hdf5_tools
+from opencsp.common.lib.tool import hdf5_tools
 
 
 @dataclass
-class ParamsSofastFringe:
+class ParamsSofastFringe(hdf5_tools.HDF5_IO_Abstract):
     """Parameters for SofastFringe processing calculation"""
 
     mask_hist_thresh: float = 0.5
@@ -52,3 +51,31 @@ class ParamsSofastFringe:
         hdf5_tools.save_hdf5_datasets(data, datasets, file)
 
         self.geometry_params.save_to_hdf(file, prefix + 'ParamsSofastFringe/')
+
+    @classmethod
+    def load_from_hdf(cls, file: str, prefix: str = ''):
+        """Loads data from given file. Assumes data is stored as: PREFIX + ParamsSofastFringe/Field_1
+
+        Parameters
+        ----------
+        file : str
+            HDF file to load from
+        prefix : str, optional
+            Prefix to append to folder path within HDF file (folders must be separated by "/").
+            Default is empty string ''.
+        """
+        # Load geometry parameters
+        params_geometry = ParamsOpticGeometry.load_from_hdf(file, prefix + '/ParamsSofastFringe/')
+
+        # Load sofast parameters
+        datasets = [
+            prefix + 'ParamsSofastFringe/mask_hist_thresh',
+            prefix + 'ParamsSofastFringe/mask_filt_width',
+            prefix + 'ParamsSofastFringe/mask_filt_thresh',
+            prefix + 'ParamsSofastFringe/mask_thresh_active_pixels',
+            prefix + 'ParamsSofastFringe/mask_keep_largest_area',
+        ]
+        data = hdf5_tools.load_hdf5_datasets(datasets, file)
+        data['geometry_params'] = params_geometry
+
+        return cls(**data)
