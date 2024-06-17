c4cc0f0dd45488e1bede377dd25fcbde62a9e6b1
Braden <bsmith4@sandia.gov>
2024-06-17 10:29:53 -0600

    Fixed typo in ProcessSofastFixed facet data save to hdf

--- opencsp/app/sofast/lib/ProcessSofastFixed.py
+++ opencsp/app/sofast/lib/ProcessSofastFixed.py
@@ -247,7 +247,7 @@ class ProcessSofastFixed(HDF5_SaveAbstract):
         # Sofast input
         self.params.save_to_hdf(file, f'{prefix:s}DataSofastInput/')
         self.data_surface.save_to_hdf(file, f'{prefix:s}DataSofastInput/optic_definition/facet_000/')
-        self.data_facet.save_to_hdf(file, f'{prefix:s}DataSofastInput/optic_definition/facet_000')
+        self.data_facet.save_to_hdf(file, f'{prefix:s}DataSofastInput/optic_definition/facet_000/')
 
         # General
         self.data_error.save_to_hdf(file, f'{prefix:s}DataSofastCalculation/general/')
