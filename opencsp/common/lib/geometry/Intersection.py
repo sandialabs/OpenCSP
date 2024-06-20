2216f5cae7e1876062d62d0dcc308fa7e0bcfa96
mhhwang <mhhwang@sandia.gov>
2024-06-20 11:46:31 -0600

    added unit tests and updated Intersection.py to include plane_lines_intersection

--- opencsp/common/lib/geometry/Intersection.py
+++ opencsp/common/lib/geometry/Intersection.py
@@ -9,6 +9,8 @@ import numpy as np
 import psutil
 from scipy.spatial.transform import Rotation
 
+import opencsp.common.lib.tool.log_tools as lt
+
 from opencsp.common.lib.csp import LightPath as lp
 from opencsp.common.lib.csp.LightPath import LightPath
 from opencsp.common.lib.csp.LightPathEnsemble import LightPathEnsemble
@@ -128,14 +130,26 @@ class Intersection:
     plane_intersec_vec = plane_intersect_from_ray_trace
 
     # TODO TJL:for maddie, make this better
-    def _from_ray_trace_vec_maddie(
+
+    @classmethod
+    def plane_lines_intersection(
+        cls,
         lines: tuple[Pxyz, Vxyz],
         plane: tuple[Pxyz, Uxyz],  # used to be --> plane_point: Pxyz, plane_normal_vector: Uxyz,
         epsilon: float = 1e-6,
         verbose: bool = False,
-    ):
+    ) -> Pxyz:
         """Vectorized plane intersection algorithm
-        place = (plane_point, plane_normal_vector)"""
+        plane = (plane_point, plane_normal_vector)
+            line intersection algorithm
+        lines = (points, directions)
+
+        Returns
+        -------
+        intersection_points: Pxyz
+            The intersection (x,y,z) locations for each of the lines with the plane, one per line. Shape (3,N), where N is the number of lines.
+            Disregards direction of line.
+        """
 
         # Unpack plane
         plane_point, plane_normal_vector = plane
@@ -143,17 +157,34 @@ class Intersection:
         # Unpack lines
         points, directions = lines
 
-        # finds where the light intersects the plane
-        # algorithm explained at \opencsp\doc\IntersectionWithPlaneAlgorithm.pdf
-        # TODO TJL:upload explicitly vectorized algorithm proof
+        # normalize inputs
+        lines_validate = np.squeeze(points.data), np.squeeze(directions.data)
+        plane_validate = np.squeeze(plane_point.data), np.squeeze(plane_normal_vector.data)
+
+        # validate inputs
+        if np.ndim(plane_validate[0].data) != 1:
+            lt.error_and_raise(
+                ValueError,
+                f"Error in plane_lines_intersection(): the 'plane' parameter should contain a single origin point, but instead contains {plane_validate[0].shape[1]} points",
+            )
+        if np.ndim(plane_validate[1].data) != 1:
+            lt.error_and_raise(
+                ValueError,
+                f"Error in plane_lines_intersection(): the 'plane' parameter should contain a single normal vector, but instead contains {plane_validate[1].shape[1]} points",
+            )
+        for i in range(directions.data.shape[1]):
+            if Vxyz.dot(plane_normal_vector, directions[i]) == 0:
+                lt.error_and_raise(
+                    ValueError,
+                    f"Error in plane_lines_intersection(): the 'plane' parameter and 'line(s)' parameter(s) are parallel.",
+                )
 
         plane_normal_vector = plane_normal_vector.normalize()
         plane_vectorV = plane_normal_vector.data  # column vector
         plane_pointV = plane_point.data  # column vector
 
         # most recent points in light path ensemble
-        if verbose:
-            print("setting up values...")
+        lt.debug("setting up values...")
         P = points.data
         V = directions.data  # current vectors
 
@@ -171,23 +202,23 @@ class Intersection:
         intersection_points = Pxyz(intersection_matrix)
 
         # filter out points that miss the plane
-        if verbose:
-            print("filtering out missed vectors")
-        filtered_intersec_points = (
-            intersection_points  # Pxyz.merge(list(filter(lambda vec: not vec.hasnan(),intersection_points)))
-        )
+        # if verbose:
+        #     print("filtering out missed vectors")
+        # filtered_intersec_points = (
+        #     intersection_points  # Pxyz.merge(list(filter(lambda vec: not vec.hasnan(),intersection_points)))
+        # )
 
-        if verbose:
-            print("Rotating.")
+        # if verbose:
+        #     print("Rotating.")
         # TODO: Do we want the inverse that takes that vector back into the up vector
         # up_vec = Vxyz([0, 0, 1])
         # rot = Vxyz.align_to(plane_normal_vector, up_vec)  # .inv()
         # rotated_intersec_points: Pxyz = filtered_intersec_points.rotate(rot)
 
-        if verbose:
-            print("Plane intersections caluculated.")
+        # if verbose:
+        #     print("Plane intersections caluculated.")
 
-        return filtered_intersec_points
+        return intersection_points
         # return np.histogram2d(xyz[:,0], xyz[:,1], bins)
         # TODO TJL:create the histogram from this or bin these results
 
