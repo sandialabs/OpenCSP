0ee90ed1d103668c63951e4b42118ca8d0b4eb08
Smith <bsmith4@sandia.gov>
2024-08-28 14:09:23 -0600

    Fixed typo. Added gain as option to live view mono basler example.

--- example/camera_io/live_view_mono_Basler.py
+++ example/camera_io/live_view_mono_Basler.py
@@ -12,7 +12,7 @@ from opencsp.common.lib.camera.LiveView import LiveView
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='run_and_save_images_Basler_color', description='Shows live view from Basler monochrome camera.'
+        prog='live_view_mono_Basler', description='Shows live view from Basler monochrome camera.'
     )
     parser.add_argument(
         'camera_index',
@@ -21,24 +21,29 @@ def main():
         help='Camera index (0-indexed in order of camera serial number) to run.',
     )
     parser.add_argument('--calibrate', action='store_true', help='calibrate camera exposure before capture')
-    parser.add_argument('-e', metavar='exposure', type=float, default=None, help='Camera exposure value')
+    parser.add_argument('-e', metavar='exposure', type=int, default=None, help='Camera exposure value')
+    parser.add_argument('-g', metavar='gain', type=int, default=None, help='Camera gain value')
     args = parser.parse_args()
 
     # Connect to camera
     cam = ImageAcquisitionMono(args.camera_index, 'Mono12')
 
     # Calibrate exposure and set frame rate
-    cam.frame_rate = 10
     if args.calibrate:
         cam.calibrate_exposure()
 
     if args.e is not None:
         cam.exposure_time = args.e
 
-    print('Exposure time:', cam.exposure_time)
-    print('Frame rate:', cam.frame_rate)
-    print('Gain:', cam.gain)
-    print('')
+    if args.g is not None:
+        print(args.g)
+        cam.gain = args.g
+
+    print('Exposure time range: ', cam.cap.ExposureTimeRaw.Min, '-', cam.cap.ExposureTimeRaw.Max)
+    print('Gain range: ', cam.cap.GainRaw.Min, '-', cam.cap.GainRaw.Max)
+
+    print('Exposure time set:', cam.exposure_time)
+    print('Gain set:', cam.gain)
 
     # Show live view image
     LiveView(cam, highlight_saturation=False)
