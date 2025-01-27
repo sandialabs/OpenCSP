dd2a1d2afd9325d2703ced963204c3524080497a
Evan Harvey <eharvey@sandia.gov>
2025-01-27 13:56:25 -0700

    Fix RTD build

--- opencsp/common/lib/process/lib/ProcessOutputLine.py
+++ opencsp/common/lib/process/lib/ProcessOutputLine.py
@@ -5,22 +5,13 @@ from dataclasses import dataclass
 class ProcessOutputLine:
     """
     Represents a single line of stdout or stderr output.
-
-    Attributes
-    ----------
-    val : str
-        The value of the line of output.
-    is_err : bool, optional
-        Whether this line originated from stderr. Defaults to False.
-    lineno : int, optional
-        The index of this line from the mixed stderr/stdout of a process. Defaults to 0.
     """
 
     # "ChatGPT 4o" assisted with generating this docstring.
 
     val: str
-    """ The value of the line """
+    """ The value of the line of output """
     is_err: bool = False
-    """ Whether this line originated from stderr. """
+    """ Whether this line originated from stderr. (Default False) """
     lineno: int = 0
-    """ The index of this line from the mixed stderr/stdout of a process. """
+    """ The index of this line from the mixed stderr/stdout of a process. (Default 0)"""
