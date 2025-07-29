ee9538b6b82f88c0c8e07fca8f41e31baba77697
Benjamin Bean <bbean@sandia.gov>
2025-07-29 10:14:18 -0600

    adding __init__.py and LazyLoader to improve load times of OpenCSP by ~.5s

--- contrib/common/lib/cv/spot_analysis/image_processor/__init__.py
+++ contrib/common/lib/cv/spot_analysis/image_processor/__init__.py
@@ -1,26 +1,41 @@
-from contrib.common.lib.cv.spot_analysis.image_processor.BackgroundColorSubtractionImageProcessor import (
-    BackgroundColorSubtractionImageProcessor,
-)
-from contrib.common.lib.cv.spot_analysis.image_processor.ColorConversionImageProcessor import (
-    ColorConversionImageProcessor,
-)
-from contrib.common.lib.cv.spot_analysis.image_processor.CustomSimpleImageProcessor import CustomSimpleImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.EnclosedEnergyImageProcessor import (
-    EnclosedEnergyImageProcessor,
-)
-from contrib.common.lib.cv.spot_analysis.image_processor.InpaintImageProcessor import InpaintImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.MomentsImageProcessor import MomentsImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.PowerpointImageProcessor import PowerpointImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.SaveToFileImageProcessor import SaveToFileImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.SpotWidthImageProcessor import SpotWidthImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.StabilizationImageProcessor import StabilizationImageProcessor
-from contrib.common.lib.cv.spot_analysis.image_processor.TargetBoardLocatorImageProcessor import (
-    TargetBoardLocatorImageProcessor,
-)
-from contrib.common.lib.cv.spot_analysis.image_processor.ViewAnnotationsImageProcessor import (
-    ViewAnnotationsImageProcessor,
-)
-from contrib.common.lib.cv.spot_analysis.image_processor.ViewHighlightImageProcessor import ViewHighlightImageProcessor
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+from opencsp import LazyLoader as _LazyLoader
+
+base_package = "contrib.common.lib.cv.spot_analysis.image_processor."
+
+# fmt: off
+BackgroundColorSubtractionImageProcessor = _LazyLoader(base_package + "BackgroundColorSubtractionImageProcessor", "BackgroundColorSubtractionImageProcessor")
+ColorConversionImageProcessor = _LazyLoader(base_package + "ColorConversionImageProcessor", "ColorConversionImageProcessor")
+CustomSimpleImageProcessor = _LazyLoader(base_package + "CustomSimpleImageProcessor", "CustomSimpleImageProcessor")
+EnclosedEnergyImageProcessor = _LazyLoader(base_package + "EnclosedEnergyImageProcessor", "EnclosedEnergyImageProcessor")
+InpaintImageProcessor = _LazyLoader(base_package + "InpaintImageProcessor", "InpaintImageProcessor")
+MomentsImageProcessor = _LazyLoader(base_package + "MomentsImageProcessor", "MomentsImageProcessor")
+PowerpointImageProcessor = _LazyLoader(base_package + "PowerpointImageProcessor", "PowerpointImageProcessor")
+SaveToFileImageProcessor = _LazyLoader(base_package + "SaveToFileImageProcessor", "SaveToFileImageProcessor")
+SpotWidthImageProcessor = _LazyLoader(base_package + "SpotWidthImageProcessor", "SpotWidthImageProcessor")
+StabilizationImageProcessor = _LazyLoader(base_package + "StabilizationImageProcessor", "StabilizationImageProcessor")
+TargetBoardLocatorImageProcessor = _LazyLoader(base_package + "TargetBoardLocatorImageProcessor", "TargetBoardLocatorImageProcessor")
+ViewAnnotationsImageProcessor = _LazyLoader(base_package + "ViewAnnotationsImageProcessor", "ViewAnnotationsImageProcessor")
+ViewHighlightImageProcessor = _LazyLoader(base_package + "ViewHighlightImageProcessor", "ViewHighlightImageProcessor")
+# fmt: on
+
+if _TYPE_CHECKING:
+    # fmt: off
+    from contrib.common.lib.cv.spot_analysis.image_processor.BackgroundColorSubtractionImageProcessor import BackgroundColorSubtractionImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.ColorConversionImageProcessor import ColorConversionImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.CustomSimpleImageProcessor import CustomSimpleImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.EnclosedEnergyImageProcessor import EnclosedEnergyImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.InpaintImageProcessor import InpaintImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.MomentsImageProcessor import MomentsImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.PowerpointImageProcessor import PowerpointImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.SaveToFileImageProcessor import SaveToFileImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.SpotWidthImageProcessor import SpotWidthImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.StabilizationImageProcessor import StabilizationImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.TargetBoardLocatorImageProcessor import TargetBoardLocatorImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.ViewAnnotationsImageProcessor import ViewAnnotationsImageProcessor
+    from contrib.common.lib.cv.spot_analysis.image_processor.ViewHighlightImageProcessor import ViewHighlightImageProcessor
+    # fmt: on
 
 # Make these classes available when importing cv.spot_analysis.image_processor.*
 __all__ = [
--- opencsp/common/lib/cv/spot_analysis/image_processor/__init__.py
+++ opencsp/common/lib/cv/spot_analysis/image_processor/__init__.py
@@ -1,40 +1,49 @@
-from opencsp.common.lib.cv.spot_analysis.image_processor.AbstractAggregateImageProcessor import (
-    AbstractAggregateImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.AbstractSpotAnalysisImageProcessor import (
-    AbstractSpotAnalysisImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.AbstractVisualizationImageProcessor import (
-    AbstractVisualizationImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.AverageByGroupImageProcessor import (
-    AverageByGroupImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.BcsLocatorImageProcessor import BcsLocatorImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.ConvolutionImageProcessor import ConvolutionImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.CroppingImageProcessor import CroppingImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.EchoImageProcessor import EchoImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.ExposureDetectionImageProcessor import (
-    ExposureDetectionImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.ViewFalseColorImageProcessor import (
-    ViewFalseColorImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.HotspotImageProcessor import HotspotImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.LogScaleImageProcessor import LogScaleImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.NullImageSubtractionImageProcessor import (
-    NullImageSubtractionImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.PopulationStatisticsImageProcessor import (
-    PopulationStatisticsImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.SupportingImagesCollectorImageProcessor import (
-    SupportingImagesCollectorImageProcessor,
-)
-from opencsp.common.lib.cv.spot_analysis.image_processor.View3dImageProcessor import View3dImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.ViewCrossSectionImageProcessor import (
-    ViewCrossSectionImageProcessor,
-)
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+from opencsp import LazyLoader as _LazyLoader
+
+base_package = "opencsp.common.lib.cv.spot_analysis.image_processor."
+
+# fmt: off
+AbstractAggregateImageProcessor = _LazyLoader(base_package + "AbstractAggregateImageProcessor", "AbstractAggregateImageProcessor")
+AbstractSpotAnalysisImageProcessor = _LazyLoader(base_package + "AbstractSpotAnalysisImageProcessor", "AbstractSpotAnalysisImageProcessor")
+AbstractVisualizationImageProcessor = _LazyLoader(base_package + "AbstractVisualizationImageProcessor", "AbstractVisualizationImageProcessor")
+AverageByGroupImageProcessor = _LazyLoader(base_package + "AverageByGroupImageProcessor", "AverageByGroupImageProcessor")
+BcsLocatorImageProcessor = _LazyLoader(base_package + "BcsLocatorImageProcessor", "BcsLocatorImageProcessor")
+ConvolutionImageProcessor = _LazyLoader(base_package + "ConvolutionImageProcessor", "ConvolutionImageProcessor")
+CroppingImageProcessor = _LazyLoader(base_package + "CroppingImageProcessor", "CroppingImageProcessor")
+EchoImageProcessor = _LazyLoader(base_package + "EchoImageProcessor", "EchoImageProcessor")
+ExposureDetectionImageProcessor = _LazyLoader(base_package + "ExposureDetectionImageProcessor", "ExposureDetectionImageProcessor")
+ViewFalseColorImageProcessor = _LazyLoader(base_package + "ViewFalseColorImageProcessor", "ViewFalseColorImageProcessor")
+HotspotImageProcessor = _LazyLoader(base_package + "HotspotImageProcessor", "HotspotImageProcessor")
+LogScaleImageProcessor = _LazyLoader(base_package + "LogScaleImageProcessor", "LogScaleImageProcessor")
+NullImageSubtractionImageProcessor = _LazyLoader(base_package + "NullImageSubtractionImageProcessor", "NullImageSubtractionImageProcessor")
+PopulationStatisticsImageProcessor = _LazyLoader(base_package + "PopulationStatisticsImageProcessor", "PopulationStatisticsImageProcessor")
+SupportingImagesCollectorImageProcessor = _LazyLoader(base_package + "SupportingImagesCollectorImageProcessor", "SupportingImagesCollectorImageProcessor")
+View3dImageProcessor = _LazyLoader(base_package + "View3dImageProcessor", "View3dImageProcessor")
+ViewCrossSectionImageProcessor = _LazyLoader(base_package + "ViewCrossSectionImageProcessor", "ViewCrossSectionImageProcessor")
+# fmt: on
+
+if _TYPE_CHECKING:
+    # fmt: off
+    from opencsp.common.lib.cv.spot_analysis.image_processor.AbstractAggregateImageProcessor import AbstractAggregateImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.AbstractSpotAnalysisImageProcessor import AbstractSpotAnalysisImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.AbstractVisualizationImageProcessor import AbstractVisualizationImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.AverageByGroupImageProcessor import AverageByGroupImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.BcsLocatorImageProcessor import BcsLocatorImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.ConvolutionImageProcessor import ConvolutionImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.CroppingImageProcessor import CroppingImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.EchoImageProcessor import EchoImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.ExposureDetectionImageProcessor import ExposureDetectionImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.ViewFalseColorImageProcessor import ViewFalseColorImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.HotspotImageProcessor import HotspotImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.LogScaleImageProcessor import LogScaleImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.NullImageSubtractionImageProcessor import NullImageSubtractionImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.PopulationStatisticsImageProcessor import PopulationStatisticsImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.SupportingImagesCollectorImageProcessor import SupportingImagesCollectorImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.View3dImageProcessor import View3dImageProcessor
+    from opencsp.common.lib.cv.spot_analysis.image_processor.ViewCrossSectionImageProcessor import ViewCrossSectionImageProcessor
+    # fmt: on
 
 # Make these classes available when importing cv.spot_analysis.image_processor.*
 __all__ = [
