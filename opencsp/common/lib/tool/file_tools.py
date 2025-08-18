ced9d7592afbcaf48191e0f28713580be144e805
Jeff Sharpe <jhsharp@sandia.gov>
2025-08-18 15:16:06 -0600

    Formatting

--- opencsp/common/lib/tool/file_tools.py
+++ opencsp/common/lib/tool/file_tools.py
@@ -965,7 +965,9 @@ def copy_file(input_dir_body_ext: str, output_dir: str, output_body_ext: str = N
     return output_body_ext
 
 
-def get_temporary_file(suffix: str = None, dir: str = None, text: bool = True, include_file_handle: bool = True) -> tuple[int, str]:
+def get_temporary_file(
+    suffix: str = None, dir: str = None, text: bool = True, include_file_handle: bool = True
+) -> tuple[int, str]:
     """
     Creates a temporary file to write to. Example usage::
 
@@ -1009,10 +1011,10 @@ def get_temporary_file(suffix: str = None, dir: str = None, text: bool = True, i
 
     if not success:
         raise FileNotFoundError(f"Could not create a tempory file in the directory '{dirname}'!")
-    
+
     if not include_file_handle:
-         os.close(fd)
-         fd = None
+        os.close(fd)
+        fd = None
 
     return fd, fname
 
