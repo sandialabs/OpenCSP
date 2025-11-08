442d10cf81401b255441b8e0925a04e806867f75
rcbrost <rcbrost@sandia.gov>
2025-11-08 05:27:41 -0700

    Fixes required to have OpenCSP pass all unit tests, and all but one example.

--- opencsp/common/lib/cv/spot_analysis/image_processor/CroppingImageProcessor.py
+++ opencsp/common/lib/cv/spot_analysis/image_processor/CroppingImageProcessor.py
@@ -123,17 +123,33 @@ class CroppingImageProcessor(AbstractSpotAnalysisImageProcessor):
 
     def validate_center(self, center_xy: tuple[int, int], operable: SpotAnalysisOperable | None, debug_name: str):
         center_x, center_y = center_xy
-        err_msg = (
-            "Error in CroppingImageProcessor.crop_around_location(): "
-            + f"centered location ({center_x}, {center_y}) is out of image bounds (width: {w}, height: {h}) "
-            + f"for {debug_name}"
-        )
+        # TODO RCB WHY IS THIS BUG HERE?  w AND h REFERENCED BEFORE SET.  HOW DID THIS HAPPEN?
+        # TODO RCB ORIGINAL CODE BELOW
+        # err_msg = (
+        #     "Error in CroppingImageProcessor.crop_around_location(): "
+        #     + f"centered location ({center_x}, {center_y}) is out of image bounds (width: {w}, height: {h}) "
+        #     + f"for {debug_name}"
+        # )
 
+        # # verify that the center is inside the image boundaries
+        # if center_x < 0 or center_x >= w or center_y < 0 or center_y >= h:
+        #     lt.error_and_raise(RuntimeError, err_msg)
+        # if operable is not None:
+        #     (h, w), _ = it.dims_and_nchannels(operable.primary_image.nparray)
+        #     if center_x >= w or center_y >= h:
+        #         lt.error_and_raise(RuntimeError, err_msg)
+
+        # TODO RCB REVISED CODE BELOW
         # verify that the center is inside the image boundaries
-        if center_x < 0 or center_x >= w or center_y < 0 or center_y >= h:
-            lt.error_and_raise(RuntimeError, err_msg)
+        # if center_x < 0 or center_x >= w or center_y < 0 or center_y >= h:
+        #     lt.error_and_raise(RuntimeError, err_msg)
         if operable is not None:
             (h, w), _ = it.dims_and_nchannels(operable.primary_image.nparray)
+            err_msg = (
+                "Error in CroppingImageProcessor.crop_around_location(): "
+                + f"centered location ({center_x}, {center_y}) is out of image bounds (width: {w}, height: {h}) "
+                + f"for {debug_name}"
+            )
             if center_x >= w or center_y >= h:
                 lt.error_and_raise(RuntimeError, err_msg)
 
