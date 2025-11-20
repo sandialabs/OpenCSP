e537757fbcb46dbf36f0b48d943858d2a593c8f8
Jeff Sharpe <jhsharp@sandia.gov>
2025-11-20 09:21:27 -0700

    Revert some logic in PowerpointImage since I already have another feature branch where I am working on that

--- opencsp/common/lib/render/lib/PowerpointImage.py
+++ opencsp/common/lib/render/lib/PowerpointImage.py
@@ -28,11 +28,9 @@ class PowerpointImage(pps.PowerpointShape):
         6. Clean up temporary files with :py:meth:`clear_tmp_save`
     """
 
-    _tmp_save_path = ft.join(orp.opencsp_temporary_dir(), "PowerpointImage/images/tmp")  # TODO jhs, revisit this logic
-    # _tmp_save_path = ft.join("C:/ctemp", "PowerpointImage/images/tmp")
-    # _tmp_save_path = ft.join("C:/ctemp", "OpenCSP_example_data/enclosed_energy/HeliostatsSpotSize/3_Process/PowerPointFigures")
+    _tmp_save_path = ft.join(orp.opencsp_temporary_dir(), "PowerpointImage/images/tmp")
     # _tmp_save_path = ft.join(
-    # "C:/ctemp", "OpenCSP_example_data/target_identification/HeliostatsSpotSize/3_Process/PowerPointFigures"
+        # "C:/ctemp", "OpenCSP_example_data/target_identification/HeliostatsSpotSize/3_Process/PowerPointFigures"
     # )
 
     def __init__(
