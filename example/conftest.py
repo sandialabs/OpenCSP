0b10195b2c2c91c24e878d112c3ef3f2d4c2e9f2
rcbrost <rcbrost@sandia.gov>
2025-12-06 11:45:10 -0700

    Finished upgrading example_scene_reconstruction.py to accept arguments either for command line or pytest.

--- example/conftest.py
+++ example/conftest.py
@@ -1,19 +1,34 @@
+"""
+Setup pytest for running examples.
+"""
+
 import pytest
 
 
 #
-# Ensure pytest adds root directory to the system path.
+# Add pytest command-line arguments supported by examples.
 #
 def pytest_addoption(parser):
-    parser.addoption('--dir-input', action='store', default='', help='Base directory with data input')
-    parser.addoption('--dir-output', action='store', default='', help='Base directory where output will be written')
+    parser.addoption('--dir_input', action='store', default='', help='Base directory with data input')
+    parser.addoption('--dir_output', action='store', default='', help='Base directory where output will be written')
+    parser.addoption(
+        '--write_full_data',
+        action='store',
+        default='False',
+        help='If true, write out a directory structure including all input data.  Otherwise only write generated output.',
+    )
 
 
 @pytest.fixture
 def dir_input_fixture(request):
-    return request.config.getoption('--dir-input')
+    return request.config.getoption('--dir_input')
 
 
 @pytest.fixture
 def dir_output_fixture(request):
-    return request.config.getoption('--dir-output')
+    return request.config.getoption('--dir_output')
+
+
+@pytest.fixture
+def write_full_data_fixture(request):
+    return request.config.getoption('--write_full_data')
--- example/scene_reconstruction/example_scene_reconstruction.py
+++ example/scene_reconstruction/example_scene_reconstruction.py
@@ -1,5 +1,87 @@
-from os.path import join, dirname
+r"""("r" prefix to ignore escape characters within docstring.)
 
+Exercise scene reconstruction algorithms.
+
+Supports these use cases:
+
+1. Pytest execution.
+   Purpose:  To verify code is still operating properly.
+
+   a. Quick pytest unit tests.  Run pytest from opencsp directory.
+      In OpenCSP directory:
+      pytest
+
+   b. More detailed pytest examples.
+      In OpenCSP\example directory:
+      pytest
+
+   c. Automated tests for pull request management.
+
+   d. Automated tests for nightly and weekly function checks.
+      Run pytest from opencsp/example directory (with arguments to run full-scale data).
+
+   e. Run pytest on only this example.
+      In OpenCSP\example directory:
+      pytest .\scene_reconstruction\example_scene_reconstruction.py
+
+   f. Using pytest as the vehicle for full-scale example execution.
+      In OpenCSP\example directory:
+      pytest .\scene_reconstruction\example_scene_reconstruction.py --dir_input=C:\ctemp\OpenCSP_ctemp\example_data_large\scene_reconstruction\data_measurement --dir_output=C:\ctemp\OpenCSP_ctemp\example_data_large\scene_reconstruction\output  --write_full_data=True
+
+   g. Using pytest as the vehicle for example execution on user data.
+      In OpenCSP\example directory:
+      pytest .\scene_reconstruction\example_scene_reconstruction.py --dir_input=<user_input_dir> --dir_output=<user_output_dir> --write_full_data=<user_choice_or_omit_argument>
+
+2. Running the example from the command line.
+   Purpose:  To apply the example calculation to new data.
+
+   a. With no arguments, to execute default behavior (for practice and study).
+      In OpenCSP\example\scene_reconstruction directory:
+      python .\example_scene_reconstruction.py
+
+   b. With an argument to read a user-specified settings file to define input sources, output locations, and execution details.
+      i. Data source/sink options:
+         (1) Data built into the repository.  No argument.
+             In OpenCSP\example\scene_reconstruction directory:
+             python .\example_scene_reconstruction.py
+         (2) Community data on local machine.  myfile_ctemp.ini
+             In OpenCSP\example\scene_reconstruction directory:
+             python .\example_scene_reconstruction.py --settings_dir_body_ext "C:\ctemp\OpenCSP_ctemp\example_scene_reconstruction_settings_ctemp.ini"
+         (3) Data on local machine, but in user-owned location.  myfile_<user_id>.ini
+             In OpenCSP\example\scene_reconstruction directory:
+             python .\example_scene_reconstruction.py --settings_dir_body_ext "C:\Users\<user_id>\OpenCSP\OpenCSP_<user_id>\example_scene_reconstruction_settings_<user_id>.ini"
+         (4) General network location.  myfile_<network_location>.ini
+             In OpenCSP\example\scene_reconstruction directory:
+             python .\example_scene_reconstruction.py --settings_dir_body_ext "\\<network_path>\OpenCSP_<net_name>\example_scene_reconstruction_settings_<net_name>.ini"
+      ii. Output levels:
+         (1) Output only newly computed information.  In .ini: write_full_data = False
+         (2) Output full beginning-to-end data corpus in a linear set of directories.
+             In .ini: write_full_data = True
+
+   c. Review command-line options:
+      In OpenCSP\example\scene_reconstruction directory:
+      python .\example_scene_reconstruction.py --help
+
+3. Calling the example calculation from other code.
+   Purpose:  To utilize the example calculation within a larger computation or application.
+
+   a. Arbitrary context and general data.
+      In calling file, import example_scenario_reconstruction, call driver.  (??other name??)
+
+4. Running an example with the Visual Studio Code debugger.
+   Purpose:  To interact with the code execution (break points, check stack variables, etc), either for study or fixing an error.  Could apply to either 2 or 3 above.
+
+   a. Use default settings, running on default data.  In VS Code, press F5 key.
+
+   b. Temporarily modify internal variable values to test specific data computation.
+      Edit in VS Code, then press F5 key.
+
+"""
+
+from os.path import join, basename, dirname
+
+import argparse
+import configparser
 import numpy as np
 
 from opencsp.app.scene_reconstruction.lib.SceneReconstruction import SceneReconstruction
@@ -10,7 +92,7 @@ import opencsp.common.lib.tool.file_tools as ft
 import opencsp.common.lib.tool.log_tools as lt
 
 
-def scene_reconstruction(dir_input, dir_output):
+def scene_reconstruction(dir_input, dir_output, write_full_data):
     """
     Reconstructs the XYZ locations of Aruco markers in a scene.
 
@@ -26,6 +108,8 @@ def scene_reconstruction(dir_input, dir_output):
         - 'alignment_points.csv': CSV file with alignment points.
     dir_output : str
         The directory where the output files, including point locations and calibration figures, will be saved.
+    write_full_data : bool
+        If true, write out a directory structure including all input data.  Otherwise only write generated output.
 
     Notes
     -----
@@ -75,30 +159,35 @@ def scene_reconstruction(dir_input, dir_output):
     # Save calibration figures
     for fig in cal_scene_recon.figures:
         figure_path_body = join(dir_output, fig.get_label() + '.png')
-        lt.info('before overwrite check, figure_path_body = ' + figure_path_body)
         # Overwrite previous versions.
         if ft.file_exists(figure_path_body):
             ft.delete_file(figure_path_body)
         fig.savefig(figure_path_body)
 
 
-def example_scene_reconstruction_driver(dir_input_fixture, dir_output_fixture):
+def example_scene_reconstruction_driver(dir_input_fixture, dir_output_fixture, write_full_data_fixture):
     """
     Sets up and runs the scene_reconstruction() routine.
 
     Parameters
     ----------
     dir_input_fixture : str
-        Directory to read input.  Called a fixture because it might be provided by pytest.
+        Directory to read input.  Has fixture suffix because it might be provided by pytest.
     dir_output_fixture : str
-        Directory to write output.  Called a fixture because it might be provided by pytest.
+        Directory to write output.  Has fixture suffix because it might be provided by pytest.
+    fixture_dir_write_full_data : bool
+        If true, write out a directory structure including all input data.  Otherwise only write generated output.
+        Has fixture suffix because it might be provided by pytest.
     """
     dir_input = join(opencsp_code_dir(), 'app/scene_reconstruction/test/data/data_measurement')
     dir_output = join(dirname(__file__), 'data/output/scene_reconstruction')
+    write_full_data = False
     if dir_input_fixture:
         dir_input = dir_input_fixture
     if dir_output_fixture:
         dir_output = dir_output_fixture
+    if write_full_data_fixture:
+        write_full_data = write_full_data_fixture
 
     # Ensure output directory is ready
     ft.create_directories_if_necessary(dir_output)
@@ -110,18 +199,15 @@ def example_scene_reconstruction_driver(dir_input_fixture, dir_output_fixture):
 
     lt.info('dir_input = ' + dir_input)
     lt.info('dir_output = ' + dir_output)
-    lt.info('Calling routine scene_reconstruction(dir_input, dir_output)...')
-    scene_reconstruction(dir_input, dir_output)
+    lt.info('write_full_data = ' + str(write_full_data))
+    lt.info('Calling routine scene_reconstruction(dir_input, dir_output, write_full_data)...')
+    scene_reconstruction(dir_input, dir_output, write_full_data)
 
 
 if __name__ == '__main__':
-    # ?? RCB SCAFFOLDING RCB -- DELETE FOLLOWING
-    import argparse
-    import configparser
-    import os
-
-    # Start argparse
-    # parser = argparse.ArgumentParser(prog=__file__.rstrip(".py"), description="Sensitive strings searcher")
+    # Parse command-line arguments, if any.
+    # Execute "python <this_file>.py --help" to see usage tips.
+    #
     # Source - https://stackoverflow.com/a
     # Posted by Martijn Pieters, modified by community. See post 'Timeline' for change history
     # Retrieved 2025-12-04, License - CC BY-SA 4.0
@@ -130,12 +216,6 @@ if __name__ == '__main__':
         description="Example scene reconstruction calculation.  Given photos with Aruco markers, find marker and camera 3-d positions.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    # parser.add_argument(
-    #     "--verbose",
-    #     action="store_true",
-    #     dest="verbose",
-    #     help="Print more information while running. Overrides '--progress'.",
-    # )
     parser.add_argument(
         "-s",
         "--settings_dir_body_ext",
@@ -145,26 +225,30 @@ if __name__ == '__main__':
         help="The directory root for reading data and writing output for this run.",
     )
     args = parser.parse_args()
-    settings_dir_body_ext: str = args.settings_dir_body_ext
-    print("arg: settings_dir_body_ext = ", settings_dir_body_ext)
-    # End argparse
+    arg_settings_dir_body_ext: str = args.settings_dir_body_ext
+    print("arg: settings_dir_body_ext = ", arg_settings_dir_body_ext)
 
-    if settings_dir_body_ext is None:
-        dir_main_input = join(opencsp_code_dir(), 'app/scene_reconstruction/test/data/data_measurement')
-        dir_main_output = join(dirname(__file__), 'data/output/scene_reconstruction')
-        write_full_data = False
+    # Get settings
+    if arg_settings_dir_body_ext is None:
+        print("Using default control settings.")
+        dir_input_main = join(opencsp_code_dir(), 'app/scene_reconstruction/test/data/data_measurement')
+        dir_output_main = join(dirname(__file__), 'data/output/scene_reconstruction')
+        write_full_data_main = False
     else:
-        print("current_working_directory = ", os.getcwd())
-        print("settings_dir_body_ext = ", settings_dir_body_ext)
-        print("ft.file_exists(settings_dir_body_ext) = ", ft.file_exists(settings_dir_body_ext))
+        print("Loading control from settings file:", arg_settings_dir_body_ext)
+        if not ft.file_exists(arg_settings_dir_body_ext):
+            print("ERROR: In " + basename(__file__) + ", settings file does not exist. Settings file:")
+            print("   ", arg_settings_dir_body_ext)
+            assert False
         settings = configparser.ConfigParser()
-        settings.read(settings_dir_body_ext)
-        dir_main_input = settings["Default"]["dir_input"]
-        dir_main_output = settings["Default"]["dir_output"]
-        write_full_data = settings["Default"]["write_full_data"]
-    # assert False
-    # ?? RCB SCAFFOLDING RCB -- END SCAFFOLDING
-    print("Before calling driver, dir_main_input  = ", dir_main_input)
-    print("Before calling driver, dir_main_output = ", dir_main_output)
-    print("Before calling driver, write_full_data = ", write_full_data)
-    example_scene_reconstruction_driver(dir_main_input, dir_main_output)
+        settings.read(arg_settings_dir_body_ext)
+        dir_input_main = settings["Default"]["dir_input"]
+        dir_output_main = settings["Default"]["dir_output"]
+        write_full_data_main = settings["Default"]["write_full_data"]
+
+    # Call driver, noting status first.
+    print("Calling driver:")
+    print("    dir_main_input  = ", dir_input_main)
+    print("    dir_main_output = ", dir_output_main)
+    print("    write_full_data = ", write_full_data_main)
+    example_scene_reconstruction_driver(dir_input_main, dir_output_main, write_full_data_main)
